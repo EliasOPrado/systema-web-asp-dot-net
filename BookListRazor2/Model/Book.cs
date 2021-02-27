d680720ec7de8f74fe436b3e6d4a26d98f58022b
EliasOPrado <eliaspradoprofessional@outlook.com>
2021-02-26 21:16:53 -0300

    initial commit

---
+++ BookListRazor2/Model/Book.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookListRazor2.Model
+{
+    public class Book
+    {
+
+        [key]
+
+        public int Id { get; set; }
+
+        [required]
+        public string Book { get; set; }
+        public string Author { get; set; }
+    }
+}
